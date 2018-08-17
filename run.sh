aa75cfdc9a246cfaa44b7b4f9533496df7d82291
mike2001 <hlu2001@163.com>
2018-08-17 09:27:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 termux-wake-lock
 cd xmrig/build
-./xmrig -a cryptonight-lite --print-time 30 --cpu-priority 1 -o 
-trtl.mine2gether.com:1115 -u
+./xmrig -a cryptonight-lite --print-time 30 --cpu-priority 1 -o trtl.pool.mine2gether.com:1115 -u
 TRTLuwanZHyUuAdkxLYoHX6tjRqdqdUBTA9LCH79PwqTdV971iWAPSqKQ2qVhYgKzAcVjwvsCdF3Y95roD4v7kbPRt7VV5nsbSm -p a1 --variant 1 -k
