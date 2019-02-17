d387bca21d122632bdf8dbf5182b7d6c44ca075c
mike2001 <hlu2001@163.com>
2019-02-17 13:26:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 termux-wake-lock
 cd xmrig/build
-nohup ./xmrig -a cryptonight_turtle --print-time 30 --cpu-priority 1 -o trtl.pool.mine2gether.com:3335 -u TRTLuwanZHyUuAdkxLYoHX6tjRqdqdUBTA9LCH79PwqTdV971iWAPSqKQ2qVhYgKzAcVjwvsCdF3Y95roD4v7kbPRt7VV5nsbSm -p a1 --variant 1 -k &
+nohup ./xmrig -a cryptonight_turtle --print-time 30 --cpu-priority 1 -o Pool.TRTL.CryptoPool.Space:3333 -u TRTLuwanZHyUuAdkxLYoHX6tjRqdqdUBTA9LCH79PwqTdV971iWAPSqKQ2qVhYgKzAcVjwvsCdF3Y95roD4v7kbPRt7VV5nsbSm -p a1 --variant 1 -k &
